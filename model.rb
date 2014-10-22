ba6d4c7e87897e5b9ac74a3728d01948b7729ff0
Rushil Lakhani <rushil@rushil-VirtualBox.(none)>
2014-10-22 12:27:08 +0100

    Borrando errores del modelo

--- model.rb
+++ model.rb
@@ -2,10 +2,9 @@ class ShortenedUrl
   include DataMapper::Resource
  
    property :id, Serial
-+  property :idusu, Text 
+   property :idusu, Text 
    property :url, Text
-+  property :to, Text
-+  
- end
+   property :to, Text
+ 
 end
 
